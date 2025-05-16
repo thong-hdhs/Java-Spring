a890ac6916ac421926c09734b6f7e2c5690372e7
Quang THi <thilq0939@ut.edu.vn>
2025-05-16 23:00:34 +0700

    Thêm khung back end java spring và thư viện tailwind

---
+++ hivapp/src/main/java/com/hivapp/uth/hivapp/HivappApplication.java
@@ -0,0 +1,13 @@
+package com.hivapp.uth.hivapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HivappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HivappApplication.class, args);
+	}
+
+}
---
+++ hivapp/src/test/java/com/hivapp/uth/hivapp/HivappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hivapp.uth.hivapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HivappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
