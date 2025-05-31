84b900b6a9dac6f5f15c39c309fd8cf4cc8c0422
Quang THi <thilq0939@ut.edu.vn>
2025-05-31 20:55:41 +0700

    navbar blog

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/navbar.component";
 
-function App() {
-
-  return (
-    <>
-      
-    </>
-  )
+const App = () => {
+    return (
+      <Routes>
+        <Route path='/' element={<Navbar/>}>
+          <Route path='signin' element={<h1>Cặc</h1>}/>
+          <Route path='signup' element={<h1>Cặc Lồn</h1>}/>
+        </Route>
+      </Routes>
+    )
 }
 
-export default App
+export default App;
