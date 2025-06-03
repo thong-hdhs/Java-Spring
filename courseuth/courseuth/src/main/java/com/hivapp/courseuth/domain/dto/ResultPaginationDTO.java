fe6da4d822373c8a8d4f3de8712fa16d220d46c2
Quang THi <thilq0939@ut.edu.vn>
2025-06-03 18:50:12 +0700

    jpa

---
+++ courseuth/courseuth/src/main/java/com/hivapp/courseuth/domain/dto/ResultPaginationDTO.java
@@ -0,0 +1,12 @@
+package com.hivapp.courseuth.domain.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+
+public class ResultPaginationDTO {
+    private Meta meta;
+    private Object result;
+}
---
+++ courseuth/courseuth/src/main/java/com/hivapp/courseuth/util/constant/GenderEnum.java
@@ -0,0 +1,5 @@
+package com.hivapp.courseuth.util.constant;
+
+public enum GenderEnum {
+FEMALE, MALE, OTHER
+}
