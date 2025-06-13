2f1a708c77edac2adcd352f296d2e34d3878bc85
Quang THi <thilq0939@ut.edu.vn>
2025-06-13 12:55:17 +0700

    hoàn thành render blog

--- src/components/blog-post.component.jsx
+++ src/components/blog-post.component.jsx
@@ -17,10 +17,10 @@ const BlogPostCard = ({content, author}) => {
         <Link to={`/api/blogs/${id}`} className="flex gap-8 items-center border-b border-grey pb-5 mb-4">
             <div className="w-full">
                     <div className="flex gap-2 items-center mb-7">
-                        <UserAvatar name={fullName} size={20} />
-                    <p className="line-clamp-1">{fullName}  @{userName}</p>
-                    <p className="min-w-fit">{getDay(published_at)}</p>
-                </div>
+                            <UserAvatar name={fullName} size={20} />
+                        <p className="line-clamp-1">{fullName}  @{userName}</p>
+                        <p className="min-w-fit">{getDay(published_at)}</p>
+                    </div>
 
                 <h1 className="blog-title">{title}</h1>
 
--- src/components/nobanner-blog-post.component.jsx
+++ src/components/nobanner-blog-post.component.jsx
@@ -1,8 +1,27 @@
-const MinimalBlogPost = () => {
+import { Link } from "react-router-dom";
+import UserAvatar from "./user-avatar";
+import { getDay } from "../common/date";
+
+const MinimalBlogPost = ({ blog1, index }) => {
+    let {title, blog_id: id, published_at, author:{personal_info:{fullName, email}}} = blog1;
+    const getUserNameFormEmail = () => {
+        return email.split('@')[0];
+    }
+
+    let userName = getUserNameFormEmail();
+    
     return (
+       <Link to={`/api/blogs/${id}`} className="flex gap-5 mb-8">
+        <h1 className="blog-index">{index<10 ? "0" + (index +1) : index +1}</h1>
         <div>
-            <h1>No Banner Blog Post</h1>
+            <div className="flex gap-2 items-center mb-7">
+                <UserAvatar name={fullName} size={20} />
+                <p className="line-clamp-1">{fullName}  @{userName}</p>
+                <p className="min-w-fit">{getDay(published_at)}</p>
+            </div>
+            <h1 className="blog-title">{title}</h1>
         </div>
+       </Link>
     )
 }
 
